c0305271831e83919a11fdb0d7f1d6b45c2815fb
Thinh Tran <tnmthu.maria98@gmail.com>
2020-05-15 21:05:55 +0700

    fix: change host

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 
-HOST = '0.0.0.0'    # Cấu hình address server
+HOST = '34.80.20.135'    # Cấu hình address server
 PORT = 8888              # Cấu hình Port sử dụng
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Cấu hình socket
 s.connect((HOST, PORT)) # tiến hành kết nối đến server
