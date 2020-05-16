b45f91d48cc8774f07faa64528f1ff8c821475b0
Thinh Tran <thinhtd2@vng.com.vn>
2020-05-16 17:57:46 +0700

    fix: update server

--- new_server.py
+++ new_server.py
@@ -1,5 +1,11 @@
-from flask import Flask, request
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './server_audio'
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -7,11 +13,33 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/uploader', methods=['POST'])
+@app.route('/uploader', methods=['POST', 'GET'])
 def upload_file():
-    f = request.files['file']
-    f.save('./server_audio/data.wav')
-    return 'file uploaded successfully'
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 
 if __name__ == '__main__':
