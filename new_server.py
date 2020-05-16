f3c2712e4262bd3ce26b49c2ac1c6a0a4d37bb17
Thinh Tran <thinhtd2@vng.com.vn>
2020-05-16 17:46:08 +0700

    fix: update server

--- new_server.py
+++ new_server.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/uploader', methods=['POST'])
+def upload_file():
+    f = request.files['file']
+    f.save(f.filename)
+    return 'file uploaded successfully'
 
 
 if __name__ == '__main__':
