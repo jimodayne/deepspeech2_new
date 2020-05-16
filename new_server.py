5d7bd3467e0e8fa6b67471cc697346fe5bfea963
Thinh Tran <thinhtd2@vng.com.vn>
2020-05-16 17:37:58 +0700

    feat: change to Flask

--- new_server.py
+++ new_server.py
@@ -1,37 +1,11 @@
-import shutil
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
-from pathlib import Path
-
-app = FastAPI()
+from flask import Flask
+app = Flask(__name__)
 
 
-def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
-    try:
-        shutil.copyfileobj(upload_file.file, destination)
-    finally:
-        upload_file.file.close()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 
-@app.get("/")
-async def main():
-    content = """
-<body>
-<form action="/uploadfile/" enctype="multipart/form-data" method="post">
-<input name="file" type="file" >
-<input type="submit">
-</form>
-</body>
-    """
-    return HTMLResponse(content=content)
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    save_upload_file(file, "./server_audio/data.wav")
-    return {"filename": file.filename}
+if __name__ == '__main__':
+    app.run()
