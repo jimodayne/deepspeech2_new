fb31479aa71ba89c7e8ea55e18227f1a7f0c1479
Thinh Tran <thinhtd2@vng.com.vn>
2020-06-11 20:52:12 +0700

    feat: update server

--- new_server.py
+++ new_server.py
@@ -57,26 +57,7 @@ def upload_file():
     </form>
     '''
 
-class Singleton:
-   __instance = None
-   @staticmethod 
-   def getInstance():
-      """ Static access method. """
-      if Singleton.__instance == None:
-         Singleton()
-      return Singleton.__instance
-   def __init__(self):
-      """ Virtually private constructor. """
-      if Singleton.__instance != None:
-         raise Exception("This class is a singleton!")
-      else:
-         Singleton.__instance = self
 
-
-@app.route('/result')
-def test():
-    a = Singleton.getInstance()
-    print(a)
 
 
 # def getVoiceToText():
